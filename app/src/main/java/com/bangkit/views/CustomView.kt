d6a968e4ecc34d664f09af06c530905b7426b3ef
annisaseptyana <starrynyteskye@gmail.com>
2022-03-05 14:07:37 +0700

    Initialization

---
+++ app/src/main/java/com/bangkit/views/CustomView.kt
@@ -0,0 +1,11 @@
+package com.bangkit.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_view)
+    }
+}
--- app/src/main/java/com/bangkit/views/MainActivity.kt
+++ app/src/main/java/com/bangkit/views/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.bangkit.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCustomView: Button = findViewById(R.id.btn_custom_view)
+        btnCustomView.setOnClickListener{
+            val moveToCustomView = Intent(this@MainActivity,CustomView::class.java)
+            startActivity(moveToCustomView)
+        }
     }
 }
